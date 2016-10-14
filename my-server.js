77ff0659c0f0fe19488eebe31a6120a66c900a66
Arthur Novaes <arthurnovaes9@gmail.com>
2016-10-14 17:13:44 -0300

    Thank you in About

--- my-server.js
+++ my-server.js
@@ -18,7 +18,7 @@ var middleware = {
 app.use(middleware.logger);
  
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('My name is Arthur and I am a student and web developer. Specially interested in Data Base.');
+	res.send('My name is Arthur and I am a student and web developer. Specially interested in Data Base. Thank you for atention!');
 });
 
 app.use(express.static(__dirname + '/public'));
